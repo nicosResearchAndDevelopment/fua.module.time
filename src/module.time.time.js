c0ab33cacbae579c96ef19ee44ed2e2939610ad7
jlangkau <jlangkau@nicos-rd.com>
2021-08-03 18:50:21 +0200

    added some times

--- src/module.time.time.js
+++ src/module.time.time.js
@@ -1,30 +1,66 @@
 const
-    _    = require('./module.time.util.js'),
-    C    = require('./module.time.constants.js'),
-    time = require('./module.time.js');
+    _     = require('./module.time.util.js'),
+    C     = require('./module.time.constants.js'),
+    time  = require('./module.time.js') // REM : grogorian/UTC
+    //
+
+    //
+    //,
+    //geol         = require('./module.time.geol.js'),
+    //anthropozean = require('./module.time.anthropozean.js'), // REM: "Anthropozän"@de
+    //ad           = require('./module.time.ad.js'), // anno domini
+;
+let times = {
+    "http://www.opengis.net/def/uom/ISO-8601/0/Gregorian": time
+};
 
 // REM draft for TRS usage
 class Time {
 
+    //#trs            = undefined;
+    //#Instant        = undefined;
+    //#ProperInterval = undefined;
+
     constructor(trs = 'http://www.opengis.net/def/uom/ISO-8601/0/Gregorian') {
+
         _.assert(_.isString(trs), 'expected trs to be a string');
+
         this.trs = trs;
         _.lockProp(this, 'trs');
+
+        // TODO :: OR
+        //this.#trs            = trs;
+        //this.#Instant        = times[trs]['Instant'];
+        //this.#ProperInterval = times[trs]['ProperInterval'];
+
     } // Time#constructor
 
     buildTemporalEntity(beginning, end) {
         let temporalEntity = null;
-        switch (this.trs) {
+        switch (this.#trs) {
             case 'http://www.opengis.net/def/uom/ISO-8601/0/Gregorian':
-                if (!end) temporalEntity = new time.Instant(beginning);
-                else temporalEntity = new time.ProperInterval(beginning, end);
+                if (!end) {
+                    temporalEntity = new time.Instant(beginning);
+                    // TODO :: OR
+                    // temporalEntity = new this.#Instant(beginning);
+                } else {
+                    temporalEntity = new time.ProperInterval(beginning, end);
+                } // if ()
                 break;
         }
-        _.assert(temporalEntity, 'Time#buildTemporalEntity : could not build a temporal entity from trs ' + this.trs);
+        _.assert(temporalEntity, 'Time#buildTemporalEntity : could not build a temporal entity from trs ' + this.#trs);
         temporalEntity.trs = this.trs;
+        // TODO :: OR
+        //temporalEntity.trs = this.#trs;
         _.lockProp(temporalEntity, 'trs');
         return temporalEntity;
     } // Time#buildTemporalEntity
+
+    // TODO : AND
+    //static setTime({'uri': uri, 'module': _module_}) {
+    //    if (!times[uri])
+    //        times[uri] = _module_;
+    //}
 
     // TODO methods
 
